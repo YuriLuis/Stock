bf61ae44bb65bc22027bad27f99cab34d92207b8
leandroschepers <leandrocompolt@gmail.com>
2019-11-29 11:11:48 -0300

    Botôes funcionando

--- app/src/main/java/com/example/stock/TelaCadastro.java
+++ app/src/main/java/com/example/stock/TelaCadastro.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 public class TelaCadastro extends AppCompatActivity {
 
     AlertDialog alerta;
+    AlertDialog alerta2;
     Button btnCadastrar;
+    EditText txtProduto, txtPrice, txtQtd, txtDesc;
+
 
     private void alert() {
         //Cria o gerador do AlertDialog
@@ -27,8 +33,22 @@ public class TelaCadastro extends AppCompatActivity {
         alerta.show();
     }
 
+    private void alerta() {
+        //Cria o gerador do AlertDialog
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+        //define o titulo
+        builder2.setTitle("StockSys");
+        //define a mensagem
+        builder2.setMessage("\n » Dados inválidos!");
+        //cria o AlertDialog
+        alerta2 = builder2.create();
+        //Exibe
+        alerta2.show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro);
@@ -42,6 +62,35 @@ public class TelaCadastro extends AppCompatActivity {
             }
 
         });
+
+        txtProduto = findViewById(R.id.txtProduto);
+        txtPrice = findViewById(R.id.txtPrice);
+        txtDesc = findViewById(R.id.txtDesc);
+        txtQtd = findViewById(R.id.txtQtd);
+
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if         ((String.valueOf(txtProduto.getText()).length() > 0 )
+                        && (String.valueOf(txtPrice.getText()).length() > 0 )
+                        && (String.valueOf(txtDesc.getText()).length() > 0 )
+                        && (String.valueOf(txtQtd.getText()).length() > 0 ))
+                {
+                    btnCadastrar.setActivated(true);
+                    alert();
+                } else {
+                    alerta();
+                }
+
+                txtProduto.setText("");
+                txtPrice.setText("");
+                txtDesc.setText("");
+                txtQtd.setText("");
+            }
+        });
+
 
 
     }
