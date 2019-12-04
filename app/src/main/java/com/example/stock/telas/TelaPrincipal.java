68a645f41f582025f2eb54078fd5133322325529
gvzao <guilherme.v@hbsis.com.br>
2019-12-04 10:51:50 -0300

    Menu concertado

--- app/src/main/java/com/example/stock/telas/TelaPrincipal.java
+++ app/src/main/java/com/example/stock/telas/TelaPrincipal.java
@@ -1,13 +1,16 @@
 package com.example.stock.telas;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.stock.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -22,7 +25,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 
-public class TelaPrincipal extends AppCompatActivity {
+public class TelaPrincipal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -36,8 +39,7 @@ public class TelaPrincipal extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               startActivity(new Intent(view.getContext(), TelaCadastro.class));
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -49,9 +51,12 @@ public class TelaPrincipal extends AppCompatActivity {
                 R.id.nav_vendedores)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -66,5 +71,20 @@ public class TelaPrincipal extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        //o id tem que ser igual ao do item do menu
+        //ai é só botar dentro do if oq fazer
+        if (id == R.id.nav_vendedores){
+
+            startActivity(new Intent(this, TelaVendedor.class));
+        }
+
+        return false;
     }
 }
